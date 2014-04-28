8ee37a309ea888712fc9f7f05b5d0fa0c43f01a9
Apua <apua.a.aa@gmail.com>
2014-04-28 10:35:49 +0800

    Modified task desc and test cases.

--- verification/tests.py
+++ verification/tests.py
@@ -11,16 +11,16 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": u"歡迎來到天龍國:http://taiwan.taipei:5566/",
-            "answer": ["http://taiwan.taipei:5566/"],
+            "input": u"https://www.google.com/ or http://lmgtfy.com/?q=keyword",
+            "answer": ["https://www.google.com/","http://lmgtfy.com/?q=keyword"],
         },
         {
-            "input": u"https://192.168.0.1/　https://127.0.0.1 <- there is an fullwidth space A___Aa",
-            "answer": ["http://192.168.0.1/交作業"],
+            "input": u'"https://192.168.0.1/", "https://127.0.0.1"',
+            "answer": ["http://192.168.0.1/","https://127.0.0.1"],
         },
         {
-            "input": u"各位同學, 這次報告要使用的server在http://192.168.0.1/交作業 ....喔抱歉不小心打錯了, 是127.0.0.1請大家>    在午夜十二點前交出 by 助教 >///<",
-            "answer": ["http://192.168.0.1/交作業"],
+            "input": u"http://taiwan.taipei/台灣在台北國裡?!",
+            "answer": ["http://taiwan.taipei/"],
         },
     ]
 }
